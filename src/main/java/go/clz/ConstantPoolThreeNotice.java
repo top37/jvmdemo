43087f4283250b5fd12341fdc2a7b5a458ce49e9
shanqiang <shanqiang@all-in-data.com>
2021-03-08 14:59:19 +0800

    gc root

---
+++ src/main/java/go/clz/ConstantPoolThreeNotice.java
@@ -0,0 +1,17 @@
+package go.clz;
+
+public class ConstantPoolThreeNotice {
+
+    final int i = 0;
+    final String str = "abc";
+
+    String fieldStr = "fff";
+
+    public void sout(String s){
+
+        int i = 0;
+
+        System.out.println(s + i);
+    }
+
+}
